b2e8ee7fdf301a441786d05c96ce9ad22db29d25
Justin Roche <jproche5@gmail.com>
2021-01-15 22:27:14 -0500

    scrape multiple pages

--- cypress/integration/examples/scrape.spec.js
+++ cypress/integration/examples/scrape.spec.js
@@ -1,45 +1,65 @@
-
 /// <reference types="cypress" />
-// const cy = require('cypress')
-function getChildText(parent,selector) {
-    const el = Cypress.$(parent).find(selector)
-    let value = 0
-    if (el.length) value = el.last().text().trim()
-    return value
+
+function getChildText(parent, selector) {
+  const el = Cypress.$(parent).find(selector);
+  let value = 0;
+  if (el.length) value = el.last().text().trim();
+  return value;
 }
 
-function getChildLink(parent,selector) {
-    const el = Cypress.$(parent).find(selector)
-    let value = 0
-    if (el.length) {
-        const last = el[0]
-        value = last.href
-    }
-    return value
+function getChildLink(parent, selector) {
+  const el = Cypress.$(parent).find(selector);
+  let value = 0;
+  if (el.length) {
+    const last = el[0];
+    value = last.href;
+  }
+  return value;
+}
+
+let results = [];
+
+function addResults(currentResults) {
+  results.push(...currentResults);
+  console.log('results length', results.length);
+}
+
+function paginate() {
+  cy.get('.pagination li').last().click();
+}
+
+function getSinglePageResults() {
+  const results = cy
+    .get('#resultsCol')
+    .children()
+    .get('.result')
+    .map((el) => {
+      return {
+        title: getChildText(el, '.jobtitle'),
+        location: getChildText(el, '.location'),
+        company: getChildText(el, '.company'),
+        salary: getChildText(el, '.salaryText'),
+        date: getChildText(el, '.date'),
+        originalLink: getChildLink(el, '.title a'),
+      };
+    })
+    .then((results) => {
+      expect(results.length).to.be.greaterThan(0);
+      console.log(results.length);
+      addResults(results);
+    });
 }
 
 context('Scrape', () => {
-    beforeEach(() => {
-        cy.visit('https://www.indeed.com/jobs?q=coding+tutor&l=')
-    })
+  beforeEach(() => {
+    cy.wrap([]).as('results');
+    cy.visit('https://www.indeed.com/jobs?q=coding+tutor&l=');
+  });
 
-    it('gets search results on one page', () => {
-        const results = cy.get('#resultsCol').
-            children()
-            .get('.result')
-            .map((el) => {
-                return {
-                    title:getChildText(el,'.jobtitle'),
-                    location:getChildText(el,'.location'),
-                    company:getChildText(el,'.company'),
-                    salary:getChildText(el,'.salaryText'),
-                    date:getChildText(el,'.date'),
-                    orinalLink:getChildLink(el,'.title a'),
-                }
-            })
-            .then((results) => {
-                expect(results.length).equal(15)
-            })
-
-    })
-})
+  it('gets search results on multiple pages', () => {
+    for (let i = 0; i < 3; i++) {
+      getSinglePageResults();
+      paginate();
+    }
+  });
+});
