4c734b2c4a2f6892392f2f9b0a0b2839411d8f93
Justin Roche <jproche5@gmail.com>
2021-01-24 21:50:30 -0500

    remove existing documents before db write

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -28,12 +28,29 @@ module.exports = (on, config) => {
             throw err;
           } else {
             let db = client.db('erb');
+            data = data.map((item) => {
+              return {
+                ...item,
+                ...{
+                  _id: {
+                    title: item.title,
+                    location: item.location,
+                    company: item.company,
+                  },
+                },
+              };
+            });
+            let ids = data.map((item) => item._id);
             switch (command) {
               case 'save':
                 db.collection('results')
-                  .insertMany(data)
-                  .then((r) => {
-                    resolve(r);
+                  .remove({ _id: { $in: ids } })
+                  .then(() => {
+                    db.collection('results')
+                      .insertMany(data)
+                      .then((r) => {
+                        resolve(r);
+                      });
                   });
                 break;
               case 'read':
